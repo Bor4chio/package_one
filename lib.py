ee72c7038a6702f24e00a06e42bd5221d3ae24f5
Daniel Hawkins <hawkind@hotmail.co.uk>
2022-05-30 16:01:03 +0100

    cupid

--- lib.py
+++ lib.py
@@ -1,4 +1,15 @@
+from random import randint
 
 
 def try_me():
     return 'get out of my package'
+
+
+def love_score(name1, name2):
+    rand_number = randint(0, 100)
+    score = ((len(name1) * len(name2) * rand_number) / (len(name1) * len(name2) * 100)) * 100
+    return f'{name1} and {name2} your love score is {score} xxx'
+
+
+def multiply_2_num(a, b):
+    return a * b
